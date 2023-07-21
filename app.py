75cbea9d970ef93ec17d7beb0ec539f6c87af557
Rahul Nagarajan <63788480+Rahul2K20@users.noreply.github.com>
2023-07-21 03:05:23 -0700

    Update UI
    Added result page

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import joblib
-import numpy as np
 
 app = Flask(__name__)
 model = joblib.load('RF_model.pkl')
-
 
 def calculate_index(val, breakpoints):
     for bp in breakpoints:
@@ -12,14 +10,7 @@ def calculate_index(val, breakpoints):
             return bp[1] * val / bp[0]
         elif val <= bp[2]:
             return bp[1] + (val - bp[0]) * (bp[3] - bp[1]) / (bp[2] - bp[0])
-
-def calculate_aqi(si, ni, rspmi, spmi):
-    return max(si, ni, rspmi, spmi)
-
-breakpoints_so2 = [(40, 50, 80, 100), (80, 100, 380, 200), (380, 200, 800, 300), (800, 300, 1600, 400), (1600, 400, float('inf'), 500)]
-breakpoints_no2 = [(40, 50, 80, 100), (80, 100, 180, 200), (180, 200, 280, 300), (280, 300, 400, 400), (400, 400, float('inf'), 500)]
-breakpoints_rspm = [(30, 50, 60, 100), (60, 100, 90, 200), (90, 200, 120, 300), (120, 300, 250, 400), (250, 400, float('inf'), 500)]
-breakpoints_spm = [(50, 50, 100, 100), (100, 100, 250, 200), (250, 200, 350, 300), (350, 300, 430, 400), (430, 400, float('inf'), 500)]
+    return bp[3] + (val - bp[2]) * (500 - bp[3]) / (bp[4] - bp[2])
 
 @app.route('/')
 def home():
@@ -27,28 +18,21 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    try:
+    try:  
         # Get the data from the POST request
-        so2 = float(request.form.get('so2'))
-        no2 = float(request.form.get('no2'))
-        rspm = float(request.form.get('rspm'))
-        spm = float(request.form.get('spm'))
-
-        # Convert raw pollutant measures to AQI values
-        SOi = calculate_index(so2, breakpoints_so2)
-        Noi = calculate_index(no2, breakpoints_no2)
-        Rpi = calculate_index(rspm, breakpoints_rspm)
-        SPmi = calculate_index(spm, breakpoints_spm)
-
-        # Make prediction using the model loaded from disk as per the data
+        SOi = float(request.form.get('so2'))
+        Noi = float(request.form.get('no2'))
+        Rpi = float(request.form.get('rspm'))
+        SPmi = float(request.form.get('spm'))
+        
         prediction = model.predict([[SOi, Noi, Rpi, SPmi]])
-
         output = prediction[0]
 
     except ValueError:
-        return jsonify("Error: Please enter valid numbers for all fields"), 400
+        return "Error: Please enter valid numbers for all fields", 400
 
-    return jsonify(output)
+    return render_template('result.html', prediction=output)
+        
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
